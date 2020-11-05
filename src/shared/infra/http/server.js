1483f7635f3ae684dcaf7164def48c92fbcaa9cc
Caio Favoretto <caiowfavoretto@gmail.com>
2020-11-05 00:32:28 -0300

    add: port envirolment variable

--- src/shared/infra/http/server.js
+++ src/shared/infra/http/server.js
@@ -1,24 +1,24 @@
-"use strict";
+require('reflect-metadata');
 
-require("reflect-metadata");
+require('dotenv/config');
 
-require("dotenv/config");
+const _express = _interopRequireDefault(require('express'));
 
-var _express = _interopRequireDefault(require("express"));
+const _cors = _interopRequireDefault(require('cors'));
 
-var _cors = _interopRequireDefault(require("cors"));
+require('express-async-errors');
 
-require("express-async-errors");
+const _AppError = _interopRequireDefault(require('../../errors/AppError'));
 
-var _AppError = _interopRequireDefault(require("../../errors/AppError"));
+const _routes = _interopRequireDefault(require('./routes'));
 
-var _routes = _interopRequireDefault(require("./routes"));
+require('../typeorm');
 
-require("../typeorm");
+require('../../container');
 
-require("../../container");
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
 
 // import rateLimiter from './middlewares/rateLimiter';
 const app = (0, _express.default)();
@@ -30,16 +30,16 @@ app.use((err, request, response, _) => {
   if (err instanceof _AppError.default) {
     return response.status(err.statusCode).json({
       status: 'error',
-      message: err.message
+      message: err.message,
     });
   }
 
   console.error(err);
   return response.status(500).json({
     status: 'error',
-    message: 'Internal server error'
+    message: 'Internal server error',
   });
 });
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('🚀 Server started on port 3333');
 });
